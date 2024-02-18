b946b4f4a2e946b87e137304b925a1cac388f266
arihito-1206 <you.get.2.burning@gmail.com>
2024-02-19 05:48:38 +0900

    :zap: Use vim-jetpack

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
 -- basic
 vim.opt.shell = "/bin/zsh"
+vim.opt.number = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
-vim.opt.number  = true
+vim.opt.textwidth = 0
+vim.opt.autoindent = true
+vim.opt.hlsearch = true
+vim.opt.clipboard = "unnamed"
+vim.cmd('syntax on')
+
+vim.cmd('packadd vim-jetpack')
+require('jetpack.plugins')
+
